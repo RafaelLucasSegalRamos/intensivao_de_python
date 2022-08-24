12ff055386d09f25fe622cd900e70ee0a5ce69ab
Rafael <94694500+RafaelLucasSegalRamos@users.noreply.github.com>
2022-08-23 21:40:51 -0300

    voltando ao normal

--- email_sender/main.py
+++ email_sender/main.py
@@ -4,18 +4,17 @@ outlook = win32.Dispatch('outlook.application')
 titulo = 'Nova menssagem sobre seu Site!'
 menssagem = "vou te hackiei kkkkk"
 nome = "rafael"
-r = 0
-for r in range(0, 100):
-    email = outlook.CreateItem(0)
 
-    email.To = "mamacosupremo.sol@gmail.com"
-    email.Subject = titulo
-    email.HTMLbody = (f"""
-                        <h1> Olá Rafael!</h1>
-                        
-                        <p>{menssagem}</p>
-                        
-                        <h2> De: </h2> <h1>{nome}</h1>
-                        """)
-    email.Send()
-    print("Email Enviado")
+email = outlook.CreateItem(0)
+
+email.To = "mamacosupremo.sol@gmail.com"
+email.Subject = titulo
+email.HTMLbody = (f"""
+                    <h1> Olá Rafael!</h1>
+                    
+                    <p>{menssagem}</p>
+                    
+                    <h2> De: </h2> <h1>{nome}</h1>
+                    """)
+email.Send()
+print("Email Enviado")
