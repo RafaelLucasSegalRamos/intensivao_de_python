6c3705decdb0d1614aa06e188745300d0511e829
Rafael <94694500+RafaelLucasSegalRamos@users.noreply.github.com>
2022-08-25 20:30:46 -0300

    UPDATE

--- email_sender/main.py
+++ email_sender/main.py
@@ -2,7 +2,7 @@ import win32com.client as win32
 
 outlook = win32.Dispatch('outlook.application')
 titulo = 'Nova menssagem sobre seu Site!'
-menssagem = "vou te hackiei kkkkk"
+menssagem = "só trocando a menssagem msm"
 nome = "rafael"
 
 email = outlook.CreateItem(0)
@@ -10,11 +10,10 @@ email = outlook.CreateItem(0)
 email.To = "mamacosupremo.sol@gmail.com"
 email.Subject = titulo
 email.HTMLbody = (f"""
-                    <h1 style="background: #222"; padding: 20px; border-radius: 20px;> Olá Rafael!</h1>
+                    <h1> Olá Rafael!</h1>
                     
                     <p>{menssagem}</p>
                     
                     <h2> De: </h2> <h1>{nome}</h1>
                     """)
 email.Send()
-print("Email Enviado")
