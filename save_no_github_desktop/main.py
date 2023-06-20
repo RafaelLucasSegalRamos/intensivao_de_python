85dc6653ccf8ca3def6fdea24f2cdc9003ee6984
Rafael <94694500+RafaelLucasSegalRamos@users.noreply.github.com>
2023-06-20 13:19:12 -0300

    update

---
+++ save_no_github_desktop/main.py
@@ -0,0 +1,25 @@
+import pyautogui
+from time import sleep
+
+
+if __name__ == '__main__':
+    pyautogui.alert('Por favor retire as mão do teclado e do mouse')
+    sleep(1)
+    pyautogui.press('win')
+    pyautogui.write('github desktop')
+    pyautogui.press('enter')
+    pyautogui.click()
+    sleep(3)
+    pyautogui.moveTo(x=2195, y=665)
+    pyautogui.click()
+    pyautogui.write('update')
+    sleep(1.5)
+    pyautogui.moveTo(x=2165, y=817)
+    pyautogui.click()
+    sleep(2)
+    pyautogui.moveTo(x=2660, y=233)
+    pyautogui.click()
+    sleep(5)
+    pyautogui.hotkey('alt', 'f4')
+    pyautogui.alert('Pode voltar as suas tarefas ao normal!')
+
