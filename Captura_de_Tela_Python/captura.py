80aa76faaa20b6e77d1e7f0c9718be1938857fea
Rafael <rafaelsegalramos@gmail.com>
2023-10-17 13:00:01 -0300

    gravando video com python

--- Captura_de_Tela_Python/captura.py
+++ Captura_de_Tela_Python/captura.py
@@ -5,8 +5,7 @@ import time
 import keyboard as kb
 
 
-fps = 15 # Por algum motivo se eu coloco qualque valor acima de 15 fps o video fica bugado e fica muito mais rápido do
-# que deveria, por exemplo quando eu coloco 60 fps, o video fica como se fosse 4x
+fps = 15 # Por algum motivo se eu coloco qualque valor acima de 15 fps o video fica bugado e fica muito mais rápido do que deveria, por exemplo quando eu coloco 60 fps, o video fica como se fosse 4x
 
 tamanho_tela = tuple(pag.size())
 
