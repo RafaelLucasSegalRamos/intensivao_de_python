fe5bf4d8d4155ca1181543d6980fc660b7e27d76
Rafael <rafaelsegalramos@gmail.com>
2023-11-21 16:52:14 -0300

    update

--- Entendendo_APIs/API_Banco_Central.py
+++ Entendendo_APIs/API_Banco_Central.py
@@ -8,22 +8,27 @@ import requests as rs
 tf = pd.DataFrame()
 pula_vlr = 0
 
-while True:
-# link = "https://olinda.bcb.gov.br/olinda/servico/mecir_dinheiro_em_circulacao/versao/v1/odata/informacoes_diarias?$top=1000&$orderby=Data%20desc&$format=json&$select=Data,Quantidade,Valor,Denominacao,Especie"
-    link = f"https://olinda.bcb.gov.br/olinda/servico/mecir_dinheiro_em_circulacao/versao/v1/odata/informacoes_diarias?$top=1000&$skip={pula_vlr}&$orderby=Data%20desc&$format=json&$select=Data,Quantidade,Valor,Denominacao,Especie"
+try:
+    while True:
+        # link = "https://olinda.bcb.gov.br/olinda/servico/mecir_dinheiro_em_circulacao/versao/v1/odata/informacoes_diarias?$top=1000&$orderby=Data%20desc&$format=json&$select=Data,Quantidade,Valor,Denominacao,Especie"
+        link = f"https://olinda.bcb.gov.br/olinda/servico/mecir_dinheiro_em_circulacao/versao/v1/odata/informacoes_diarias?$top=1000&$skip={pula_vlr}&$orderby=Data%20desc&$format=json&$select=Data,Quantidade,Valor,Denominacao,Especie"
 
-    response = rs.get(link)
-    infor = response.json()
+        response = rs.get(link)
+        infor = response.json()
 
-    tabela = pd.DataFrame(infor["value"])
-    if len(infor["value"]) < 1:
-        break
-    tf = pd.concat([tf, tabela])
-    pula_vlr += 1000
+        tabela = pd.DataFrame(infor["value"])
+        tf = pd.concat([tf, tabela])
+        pula_vlr += 10000
 
-    # tabela = pd.DataFrame(infor["value"])
-    # tabela["Data"] = pd.to_datetime(tabela["Data"])
-    # tabela["Valor"] = tabela["Valor"].map("{:,.2f}".format)
-    # tabela["Quantidade"] = tabela["Quantidade"].map("{:,}".format)
+        if not bool(tabela["value"]):
+            break
 
-print(tabela)
+
+except:
+    print("Fim da paginação")
+finally:
+    tf = pd.DataFrame(infor["value"])
+    tf["Data"] = pd.to_datetime(tf["Data"])
+    tf["Valor"] = tf["Valor"].map("{:,.2f}".format)
+    tf["Quantidade"] = tf["Quantidade"].map("{:,}".format)
+    print(tf)
